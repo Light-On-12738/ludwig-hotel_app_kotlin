e13d74af8e219ca6959e4c5fc49c7d48c5c55db5
golden737 <golden713a@gmail.com>
2026-09-14 20:12:27 -0600

    Conexion a la base de datos

---
+++ app/src/main/java/com/example/ludwighotel/SupabaseClient.kt
@@ -0,0 +1,25 @@
+package com.tuapp.data.remote
+
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.storage.Storage
+import io.github.jan.supabase.realtime.Realtime
+
+
+object SupabaseClientProvider {
+
+    
+    private const val SUPABASE_URL = "https://wnkfbohqydlrzjqttpew.supabase.co"
+    private const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indua2Zib2hxeWRscnpqcXR0cGV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5ODU4NTQsImV4cCI6MjEwNDU2MTg1NH0.yuSrsBZWl8il4dRKUD2yMfDFsf-gutL1rEJPbiqaeUU"
+
+    val client = createSupabaseClient(
+        supabaseUrl = SUPABASE_URL,
+        supabaseKey = SUPABASE_ANON_KEY
+    ) {
+        install(Postgrest)
+        install(Auth)
+        install(Storage)
+        install(Realtime)
+    }
+}
