338c225e25601812dc8684ec245ea534d6d8f405
golden737 <golden713a@gmail.com>
2026-09-15 17:01:33 -0600

    Cambios en la build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.9")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation(platform("io.github.jan-tennert.supabase:bom:3.1.4"))
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
     implementation("io.github.jan-tennert.supabase:auth-kt")
